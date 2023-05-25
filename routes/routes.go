8df3db41892323e3b4165f7ba5e59b8c2cef3cc1
Yongle Xu <yonglexu08@gmail.com>
2023-05-25 17:44:53 +0000

    Add recovery middleware from panics

--- routes/routes.go
+++ routes/routes.go
@@ -10,6 +10,7 @@ func NewRouter(a app.Application) *gin.Engine {
 	r := gin.Default()
 	r.NoMethod(MethodNotAllowed(a))
 	r.NoRoute(NotFound(a))
+	r.Use(gin.Recovery())
 	v1 := r.Group("/v1")
 	v1.GET("/healthcheck", func(c *gin.Context) {
 		handlers.HealthcheckHandler(c, a)
