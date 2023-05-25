d368d5b57eb4b04e66f46814726eca0e7bb82ab9
Yongle Xu <yonglexu08@gmail.com>
2023-05-25 22:18:28 +0000

    IP based rate limiting

--- routes/middleware.go
+++ routes/middleware.go
@@ -1,6 +1,10 @@
 package routes
 
 import (
+	"net"
+	"sync"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/rwx-yxu/greenlight/app"
 	"github.com/rwx-yxu/greenlight/handlers"
@@ -8,18 +12,66 @@ import (
 )
 
 func RateLimit(app app.Application) gin.HandlerFunc {
-	// Initialize a new rate limiter which allows an average of 2 requests per second,
-	// with a maximum of 4 requests in a single ‘burst’.
-	limiter := rate.NewLimiter(2, 4)
+	// Define a client struct to hold the rate limiter and last seen time for each
+	// client.
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+	// Declare a mutex and a map to hold the clients' IP addresses and rate limiters.
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
+
+	// Launch a background goroutine which removes old entries from the clients map once
+	// every minute.
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+
+			// Lock the mutex to prevent any rate limiter checks from happening while
+			// the cleanup is taking place.
+			mu.Lock()
+
+			// Loop through all clients. If they haven't been seen within the last three
+			// minutes, delete the corresponding entry from the map.
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+
+			// Importantly, unlock the mutex when the cleanup is complete.
+			mu.Unlock()
+		}
+	}()
+
 	return func(c *gin.Context) {
-		// Call limiter.Allow() to see if the request is permitted, and if it's not,
-		// then we call the rateLimitExceededResponse() helper to return a 429 Too Many
-		// Requests response (we will create this helper in a minute).
-		if !limiter.Allow() {
+		ip, _, err := net.SplitHostPort(c.Request.RemoteAddr)
+		if err != nil {
+			handlers.ErrorResponse(c, app, handlers.InternalServerError(err))
+			return
+		}
+		mu.Lock()
+		// Check to see if the IP address already exists in the map. If it doesn't, then
+		// initialize a new rate limiter and add the IP address and limiter to the map.
+		if _, found := clients[ip]; !found {
+			clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
+		}
+
+		// Update the last seen time for the client.
+		clients[ip].lastSeen = time.Now()
+
+		// Call the Allow() method on the rate limiter for the current IP address. If
+		// the request isn't allowed, unlock the mutex and send a 429 Too Many Requests
+		// response, just like before.
+		if !clients[ip].limiter.Allow() {
+			mu.Unlock()
 			handlers.ErrorResponse(c, app, handlers.RateLimitExceededError())
 			return
 		}
-
+		mu.Unlock()
 		c.Next()
 	}
 }
